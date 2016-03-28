a93ba9b06213859cc9e59252690604b9737eee76
Ed Henderson <ed@teamhendo.com>
2016-03-28 16:02:04 -0700

    Wire up Search API to view videos, prev/next and page through #771 - fixed favicon path - added FilterBar component (stub) - added some translations - added in locale to footer for now

---
+++ src/js/components/core/FilterBar.js
@@ -0,0 +1,40 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+import React from 'react';
+import T from '../../translation';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+var FilterBar = React.createClass({
+    handlePrevButton: function(e) {
+        e.preventDefault();
+        alert('Previous');        
+    },
+    handleNextButton: function(e) {
+        e.preventDefault();
+        alert('Next');
+    },
+    render() {
+        var self = this;
+        return (
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <div className="navbar-item">
+                        <button className="button is-primary" onClick={this.handlePrevButton}>{T.get('copy.PreviousLabel')}</button>
+                    </div>
+                </div>
+                <div className="navbar-right">
+                    <div className="navbar-item">
+                        <button className="button is-primary" onClick={this.handleNextButton}>{T.get('copy.NextLabel')}</button>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+export default FilterBar;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
