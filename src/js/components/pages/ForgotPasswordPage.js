2030d309d2b0804f37ed8a8b2e2c0e05fe9bcd4a
Stephen Caulfield <s.caulfield88@gmail.com>
2016-04-04 13:50:56 -0700

    Password Reset Page: #801
    * add ForgotPassword form + add link to Sign in form
    
    * add state TODO
    
    * add changes
    
    * add sign in link return + add Reset notificaition
    
    * fix corrections + add translation copy

--- src/js/components/pages/ForgotPasswordPage.js
+++ src/js/components/pages/ForgotPasswordPage.js
@@ -1,19 +1,21 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 import React from 'react';
+import ForgotPasswordForm from '../forms/ForgotPasswordForm';
 import SiteHeader from '../wonderland/SiteHeader';
 import SiteFooter from '../wonderland/SiteFooter';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 var ForgotPasswordPage = React.createClass({
+
     render: function() {
         return (
             <div>
                 <SiteHeader />
                 <section className="section">
                     <div className="container">
-                        TODO - ForgotPasswordPage
+                        <ForgotPasswordForm />
                     </div>
                 </section>
                 <SiteFooter />
--- src/js/components/pages/SignInPage.js
+++ src/js/components/pages/SignInPage.js
@@ -1,9 +1,11 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 import React from 'react';
+import {Link} from 'react-router';
 import SiteHeader from '../wonderland/SiteHeader';
 import SiteFooter from '../wonderland/SiteFooter';
 import SignInForm from '../forms/SignInForm';
+import T from '../../modules/translation';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -15,6 +17,7 @@ var SignInPage = React.createClass({
                 <section className="section columns">
                     <div className="column is-half is-offset-quarter">
                         <SignInForm />
+                        <Link activeClassName="active" to="/forgot/">{T.get('reset.forgot')}</Link>
                     </div>
                 </section>
                 <SiteFooter />
