56390f4206182f1aaa3fc856420794b638c907f2
David Hutchings <dhutchings@vivoinspire.com>
2016-04-04 22:47:31 -0700

    [NEON-811] Adding in "Secured" mixin

--- src/js/components/pages/VideoPage.js
+++ src/js/components/pages/VideoPage.js
@@ -4,10 +4,15 @@ import React from 'react';
 import SiteHeader from '../wonderland/SiteHeader';
 import SiteFooter from '../wonderland/SiteFooter';
 import Video from '../wonderland/Video';
+import Secured from '../../mixins/secured';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 var VideoPage = React.createClass({
+    mixins: [ Secured ],
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
     render: function() {
         return (
             <div>
