d9cb80f472cc6e9e7d7bec1dd1ed1baefb31d4c1
Ed Henderson <ed@teamhendo.com>
2016-04-06 12:42:42 -0700

    Signed In and Signed Out Nav #852 - tidied NewsFlash component to specify propType and defaultProps, returned false when its not needed - tweaked SiteHeader to reflect new NewsFlash requirements - added new SiteNavigation component which renders a SignedIn and SignedOut version

--- src/js/components/wonderland/NewsFlash.js
+++ src/js/components/wonderland/NewsFlash.js
@@ -5,12 +5,26 @@ import React from 'react';
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 var NewsFlash = React.createClass({
+    propTypes: {
+        isActive: React.PropTypes.bool
+    },
+    getDefaultProps: function() {
+        return {
+            isActive: false
+        }
+    },
     render: function() {
-        return (
-            <div className="notification is-danger is-marginless is-text-centered">
-                {this.props.message}
-            </div>
-        );
+        var self = this;
+        if (self.props.isActive) {
+            return (
+                <div className="notification is-danger is-marginless is-text-centered">
+                    {this.props.message}
+                </div>
+            );
+        }
+        else {
+            return false;
+        }
     }
 });
 
