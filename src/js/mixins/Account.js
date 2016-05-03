5bb2a493437c8dfa75f5c041eeca7523a21bc550
David Hutchings <dhutchings@vivoinspire.com>
2016-05-03 00:59:33 -0700

    Normalizing return from both Account mixing methods

--- src/js/mixins/Account.js
+++ src/js/mixins/Account.js
@@ -4,26 +4,32 @@ import AJAX from '../modules/ajax';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+function normalizeAccount(res) {
+    return {
+        // integration_ids
+        // users
+        defaultThumbnailId: res.default_thumbnail_id,
+        trackerAccountId: res.tracker_account_id,
+        accountId: res.account_id,
+        created: res.created,
+        updated: res.updated,
+        defaultWidth: res.default_size[0],
+        defaultHeight: res.default_size[1],
+        stagingTrackerAccountId: res.staging_tracker_account_id,
+        accountEmail: res.email,
+        accountName: res.customer_name,
+        isServingEnabled: res.serving_enabled ? !!res.serving_enabled : false
+    };
+}
+
 var Account = {
     getAccount: function() {
         return new Promise(function (resolve, reject) {
             AJAX.doGet('', {})
                 .then(function(res) {
-                    resolve({
-                        // integration_ids
-                        // users
-                        defaultThumbnailId: res.default_thumbnail_id,
-                        trackerAccountId: res.tracker_account_id,
-                        accountId: res.account_id,
-                        created: res.created,
-                        updated: res.updated,
-                        defaultWidth: res.default_size[0],
-                        defaultHeight: res.default_size[1],
-                        stagingTrackerAccountId: res.staging_tracker_account_id,
-                        accountEmail: res.email,
-                        accountName: res.customer_name,
-                        isServingEnabled: res.serving_enabled ? !!res.serving_enabled : false
-                    });
+                    resolve(
+                        normalizeAccount(res)
+                    );
                 })
                 .catch(function (err) {
                     reject(err);
@@ -46,8 +52,10 @@ var Account = {
                 }
             };
             AJAX.doPut('', options)
-                .then(function(json) {
-                    resolve(json);
+                .then(function(res) {
+                    resolve(
+                        normalizeAccount(res)
+                    );
                 })
                 .catch(function(err) {
                     reject(err);
