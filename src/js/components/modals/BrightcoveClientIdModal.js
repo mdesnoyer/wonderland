93e875584ccf454eed637654dafc8b9cfd0d3728
Stephen Caulfield <s.caulfield88@gmail.com>
2016-05-15 22:26:29 -0700

    Copy updates for integration wizard #1069 (#250)
    * add modal copy ++ account setting images
    * PR Feedback

--- src/js/components/modals/BrightcoveClientIdModal.js
+++ src/js/components/modals/BrightcoveClientIdModal.js
@@ -8,40 +8,43 @@ var BrightcoveClientIdModal = React.createClass({
     render: function() {
         return (
             <section className="box is-desktop">
-                <h1 className="title is-4">
-                    Brightcove Video Cloud API Authentication Credentials
-                </h1>
-                <h4 className="title is-5"></h4>
-                <p>In order to set up your API authentication credentials, go to the <a href="https://studio.brightcove.com/products/videocloud/admin/oauthsettings" rel="external">API Authentication</a> page in your Brightcove account.</p>
-                <ol>
-                    <li>Click &ldquo;Register New Application&rdquo;</li>
-                    <li>Enter &ldquo;Neon&rdquo; in the name field</li>
-                    <li>Select the following permissions:
-                        <ul>
-                            <li>CMS
-                                <ul>
-                                    <li>Notifications</li>
-                                    <li>Video Read</li>
-                                    <li>Video Read/Write</li>
-                                </ul>
-                            </li>
-                            <li>Players
-                                <ul>
-                                    <li>Read</li>
-                                    <li>Read/Write</li>
-                                </ul>
-                            </li>
-                            <li>Dynamic Ingest
-                                <ul>
-                                    <li>Create</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>Click &ldquo;Save&rdquo;</li>
-                    <li>A window will appear with your Client ID and Client Secret. Copy these values into the Neon plugin configuration page, which you can access by clicking on the gear in your Neon account and selecting Plugins.</li>
-                </ol>
-                <p>For more information about managing your API authentication in Brightcove, see Brightcove&rsquo;s documentation <a href="https://support.brightcove.com/en/video-cloud/docs/managing-api-authentication-credentials" rel="external">here</a>.</p>
+                <div className="content">
+                    <h1 className="title is-4">
+                        Brightcove Video Cloud API Authentication Credentials
+                    </h1>
+                    <h4 className="title is-5"></h4>
+                    <p>In order to set up your API authentication credentials, go to the <a href="https://studio.brightcove.com/products/videocloud/admin/oauthsettings" rel="external">API Authentication</a> page in your Brightcove account.</p>
+                    <ol>
+                        <li>Click &ldquo;Register New Application&rdquo;</li>
+                        <li>Enter &ldquo;Neon&rdquo; in the name field</li>
+                        <li>Select the following permissions:
+                            <ul>
+                                <li>CMS
+                                    <ul>
+                                        <li>Notifications</li>
+                                        <li>Video Read</li>
+                                        <li>Video Read/Write</li>
+                                    </ul>
+                                </li>
+                                <li>Players
+                                    <ul>
+                                        <li>Read</li>
+                                        <li>Read/Write</li>
+                                    </ul>
+                                </li>
+                                <li>Dynamic Ingest
+                                    <ul>
+                                        <li>Create</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Click &ldquo;Save&rdquo;</li>
+                        <li>A window will appear with your Client ID and Client Secret. Copy these values into the Neon plugin configuration page, which you can access by clicking on the gear in your Neon account and selecting Plugins.</li>
+                    </ol>
+                    <p>For more information about managing your API authentication in Brightcove, see Brightcove&rsquo;s documentation <a href="https://support.brightcove.com/en/video-cloud/docs/managing-api-authentication-credentials" rel="external">here</a>.</p>
+                    <p>Learn about why we need these permissions <a href="/support/#brightcove-plugin-guide">here</a>.</p>
+                </div>
             </section>
         );
     }
--- src/js/components/modals/BrightcoveSmartPlayerModal.js
+++ src/js/components/modals/BrightcoveSmartPlayerModal.js
@@ -8,23 +8,29 @@ var BrightcoveSmartPlayerModal = React.createClass({
     render: function() {
         return (
             <section className="box is-desktop">
-              <h1 className="title is-4">Legacy Smart Player</h1>
-                <p>To configure a legacy Smart Player, you must add the Neon Telemetry Player URL to each player in your Brightcove account.</p>
-                <ol>
-                    <li>Visit the Plugin page in your Neon UI</li>
-                    <li>Answer &ldquo;yes&rdquo; to &ldquo;Do you use the Smart Player?&rdquo;</li>
-                    <li>Copy the Neon Telemetry Player URL to the plugin</li>
-                    <li>Log in to your Brightcove Account</li>
-                    <li>Go to <a href="https://videocloud.brightcove.com/publishing" rel="external">https://videocloud.brightcove.com/publishing</a></li>
-                    <li>For each player you want to add the plugin to
-                        <ol>
-                            <li>Click Settings</li>
-                            <li>In the resulting window, click &ldquo;Plug-ins&rdquo;</li>
-                            <li>Paste the Neon Telemetry Plugin URL to URL bar</li>
-                            <li>Click &ldquo;Add&rdquo;</li>
-                        </ol>
-                    </li>
-                </ol>
+                <div className="content">
+                    <h1 className="title is-4">Legacy Smart Player</h1>
+                    <p>To configure a legacy Smart Player, you must add the Neon Telemetry Player URL to each player in your Brightcove account.</p>
+                    <ol>
+                        <li>Copy the Neon Telemetry Player URL to the plugin</li>
+                        <li>Log in to your Brightcove Account</li>
+                        <li>Go to <a href="https://videocloud.brightcove.com/publishing" rel="external">https://videocloud.brightcove.com/publishing</a></li>
+                        <li>For each player you want to add the plugin to
+                            <ol>
+                                <li>Click Settings</li>
+                                <li>In the resulting window, click &ldquo;Plug-ins&rdquo;</li>
+                                <li>Paste the Neon Telemetry Plugin URL to URL bar</li>
+                                <li>Click &ldquo;Add&rdquo;</li>
+                            </ol>
+                        </li>
+                    </ol>
+                    {/*<figure className="image wonderland-image">
+                        <img src="/img/brightcoveNewPlayerModal.png" alt="" title="" />
+                    </figure>*/}
+
+                    {/*<p>(note, where will the Telemetry URL appear during this flow? Will they be able to copy the URL on the page where they clicked the question mark to open this modal? point 1 above can be updated with this information.)</p>*/}
+                    {/*<p>(also note, you might want to change the text of the question on the main plugins page to say something like "Do you use the legacy Smart Player?&rdquo; and in smaller text below it: &ldquo;If you do, please follow these instructions to add the Neon Telemetry Player Plugin to your players: [link to legacy player instructions in the brightcove guide on our support page]</p>*/}
+                </div>
             </section>
         );
     }
