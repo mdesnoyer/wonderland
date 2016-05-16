54850cd5b00c6e458aae39610a07603c7861cf7c
Ed Henderson <ed@teamhendo.com>
2016-05-16 03:33:08 -0700

    WIP

--- src/js/components/forms/SignUpForm.js
+++ src/js/components/forms/SignUpForm.js
@@ -37,11 +37,11 @@ var SignUpForm = React.createClass({
     },
     componentWillMount: function() {
         var self = this;
-        if (SESSION.active()) {
-            self.context.router.push(UTILS.DRY_NAV.DASHBOARD.URL);
-        }
         if (self.props.signUpRef) {
             cookie.save('signUpRef', self.props.signUpRef, { path: '/' });
+        }
+        if (SESSION.active()) {
+            self.context.router.push(UTILS.DRY_NAV.DASHBOARD.URL);
         }
     },
     componentWillUnmount: function(e) {
