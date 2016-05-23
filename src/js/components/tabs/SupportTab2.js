d91c479b18285e20715948141f51bc5b0f8f2f14
scaulfield01 <caulfield@neon-lab.com>
2016-05-23 11:44:51 -0700

    make copy changes to the support tab

--- src/js/components/tabs/SupportTab2.js
+++ src/js/components/tabs/SupportTab2.js
@@ -123,14 +123,14 @@ var SupportTab2 = React.createClass({
                 <ol>
                     <li>Visit the Plugin page in your Neon UI</li>
                     <li>Answer &ldquo;yes&rdquo; to &ldquo;Do you use the Smart Player?&rdquo;</li>
-                    <li>Copy the Neon Telemetry Player URL to the plugin</li>
+                    <li>Copy the Neon Smart Player Plugin URL.</li>
                     <li>Log in to your Brightcove Account</li>
                     <li>Go to <a href="https://videocloud.brightcove.com/publishing" rel="external">https://videocloud.brightcove.com/publishing</a></li>
                     <li>For each player you want to add the plugin to
                         <ol>
                             <li>Click Settings</li>
                             <li>In the resulting window, click &ldquo;Plug-ins&rdquo;</li>
-                            <li>Paste the Neon Telemetry Plugin URL to URL bar</li>
+                            <li>Paste the Neon Smart Player Plugin URL into the URL bar.</li>
                             <li>Click &ldquo;Add&rdquo;</li>
                         </ol>
                     </li>
