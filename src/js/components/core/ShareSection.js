2d91be71ea1f29fe289ddf994a2e06d7f3297472
scaulfield01 <caulfield@neon-lab.com>
2016-05-31 14:22:58 -0700

    add share package

---
+++ src/js/components/core/ShareSection.js
@@ -0,0 +1,30 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import React from 'react';
+import {ShareButtons, ShareCounts, generateShareIcon} from 'react-share';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton,
+} = ShareButtons
+
+
+
+var ShareSection = React.createClass({
+	render: function() {
+		return (
+			<div> HELLO </div>
+		)
+	}
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default ShareSection; 
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
