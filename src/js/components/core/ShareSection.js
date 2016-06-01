b879b854b23849ab19a544cc21f83f3cfc160191
scaulfield01 <s.caulfield88@gmail.com>
2016-06-01 02:04:15 -0700

    PR Changes + add stylesheet

--- src/js/components/core/ShareSection.js
+++ src/js/components/core/ShareSection.js
@@ -19,49 +19,47 @@ var ShareSection = React.createClass({
         //the variables below are used to make the formatting consistent with the React Share package
         var self = this,
             shareUrl = self.props.url,
-            title = 'Neon Image',
-            greyBackground = {fill:'#aaa'},
-            outerDivstyling = {width:'32px',height:'32px'},
-            svgStyling = {fill:'white',width:'32px',height:'32px'}
-        ; 
+            title = 'Check out this Neon Score of ' + self.props.score + '!' ,
+            customColorBackground = {fill:'#aaa'}
+        ;
         return (
-            <div className='columns container is-fluid'> 
+            <div className='columns container'>
                 <FacebookShareButton
                     url={shareUrl}
-                    title={title}
-                    className={'column is-3'}
+                    title={'I scored a '+ self.props.score +' with Neon!'}
+                    className={'column is-3 share-section-icon-color'}
                     >
                     <FacebookIcon
-                        iconBgStyle={greyBackground}
+                        iconBgStyle={customColorBackground}
                         size={32}
                         round />
                 </FacebookShareButton>
                 <TwitterShareButton
                     url={shareUrl}
-                    title={title}
-                    className={'column is-3'}
+                    title={'Thanks @neon, you rock, you got me a ' + self.props.score + '!'}
+                    className={'column is-3 share-section-icon-color'}
                      >
                     <TwitterIcon
-                        iconBgStyle={greyBackground}
+                        iconBgStyle={customColorBackground}
                         size={32}
                         round />
                 </TwitterShareButton>
                 <LinkedinShareButton
                     url={shareUrl}
-                    title={title}
-                    className={'column is-3'}
+                    title={"I scored a "+ self.props.score +" with Neon!"}
+                    className={'column is-3 share-section-icon-color'}
                      >
                     <LinkedinIcon
-                        iconBgStyle={greyBackground}
+                        iconBgStyle={customColorBackground}
                         size={32}
                         round />
                 </LinkedinShareButton>
-                <a className='column is-3' href={"mailto:?to=&subject=Check%20out%20this%20Neon%20Image&body="+self.props.url} target="_top">
-                    <div className="SocialMediaShareButton SocialMediaShareButton">
-                        <div style={outerDivstyling}>
-                            <svg viewBox="0 0 64 64" style={svgStyling} className="social-icon social-icon">
-                            <g><circle cx="32" cy="32" r="31" style={greyBackground}></circle></g> 
-                            <g><path d="M17,22v20h30V22H17z M41.1,25L32,32.1L22.9,25H41.1z M20,39V26.6l12,9.3l12-9.3V39H20z"></path></g>
+                <a href={"mailto:?to=&subject=Check%20out%20this%20Neon%20Image&body="+ title + " Link: " + self.props.url} target="_top">
+                    <div className="column is-3">
+                        <div className="share-section-div-circle-svg">
+                            <svg viewBox="0 0 64 64" className="share-section-circle-svg">
+                                <g><circle cx="32" cy="32" r="31" style={customColorBackground}></circle></g>
+                                <g><path d="M17,22v20h30V22H17z M41.1,25L32,32.1L22.9,25H41.1z M20,39V26.6l12,9.3l12-9.3V39H20z"></path></g>
                             </svg>
                         </div>
                     </div>
@@ -73,6 +71,6 @@ var ShareSection = React.createClass({
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-export default ShareSection; 
+export default ShareSection;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
