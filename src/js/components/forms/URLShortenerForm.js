058d03bf6659b8967b95f5c05aa569db7c50e79d
Alex Karim <karim@neon-lab.com>
2016-06-15 11:48:31 -0700

    no code changes on this branch for cleanliness

--- src/js/components/forms/URLShortenerForm.js
+++ src/js/components/forms/URLShortenerForm.js
@@ -35,6 +35,7 @@ var URLShortenerForm = React.createClass({
                 data: {
                     longUrl: self.refs.url.value.trim(),
                     access_token: UTILS.BITLY_ACCESS_TOKEN,
+                    domain: 'bit.ly'
                 }
             })
             .then(function(response) {
@@ -96,7 +97,7 @@ var URLShortenerForm = React.createClass({
                         <p className={'control is-grouped' + (self.state.mode === 'success' ? '' : ' is-hidden')}>
                             <output
                                 className="input small"
-                                type="url"
+                                type="text"
                                 readOnly
                                 refs="outputUrl"
                                 value={self.state.shortURL}
