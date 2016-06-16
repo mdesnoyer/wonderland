a850fcab4897cab87d6f65cbaa37a9edb1eb5019
Alex Karim <karim@neon-lab.com>
2016-06-16 12:03:28 -0700

    Brightcove thumbnail content

---
+++ src/js/components/static/BrightcoveThumbnailContent.js
@@ -0,0 +1,23 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+import React from 'react';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+var BrightcoveThumbnailContent = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <p>In order to allow Neon to serve and test the thumbnail images on your site, you need to specify our serving URL as the source of the thumbnail to display on your page. The serving URL will cause different images to be seen by different users, enabling the realtime optimization.</p>
+                <p>As a Brightcove customer, you might already be using the Brightcove Media API to determine which image to show on your site. If you&rsquo;re a Gallery customer, the default is to use the Brightcove Media API.</p>
+                <p>If you do not use the Brightcove Media API to determine the thumbnails shown on your site, then you must insert our serving URLs directly. See the <a href="/support/#custom-plugin-guide">image serving documentation</a> for more details.</p>
+            </div>
+        );
+    }
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default BrightcoveThumbnailContent;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- src/js/components/tabs/SupportTab2.js
+++ src/js/components/tabs/SupportTab2.js
@@ -7,6 +7,7 @@ import UTILS from '../../modules/utils';
 import BrightcoveAccountIdContent from '../static/BrightcoveAccountIdContent';
 import BrightcoveClientIdContent from '../static/BrightcoveClientIdContent';
 import BrightcoveSmartPlayerContent from '../static/BrightcoveSmartPlayerContent';
+import BrightcoveThumbnailContent from '../static/BrightcoveThumbnailContent';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -74,8 +75,7 @@ var SupportTab2 = React.createClass({
 
                 <h3 className="title is-4">2. Enable Image Serving</h3>
                 <p>Once you&rsquo;ve entered your Brightcove Account ID and API credentials into your Neon account, you&rsquo;re ready to move onto the next step. In this step, you&rsquo;ll ensure that Neon is able to serve images onto your site and perform realtime optimization.</p>
-                <p>To enable this functionality, you must specify our serving URL as the source of the thumbnail to display on your page. The serving URL will cause different images to be seen by different users, enabling the realtime optimization.  As a Brightcove customer, you might already be using the Brightcove Media API to determine which image to show on your site. If that is the case, you can skip this step and move on to the next step. If you&rsquo;re a Gallery customer, the default is to use the <a href="http://docs.brightcove.com/en/video-cloud/media/guides/quick-start.html" rel="external">Brightcove Media API</a>.</p>
-                <p>If you do not use the Brightcove Media API to determine the thumbnails shown on your site, then you must insert our serving URLs directly. Please see the image serving documentation for more details.</p>
+                <BrightcoveThumbnailContent />
 
                 <h3 className="title is-4">3. Enable Telemetry from Your Players</h3>
                 <p>During this step, you&rsquo;ll add the Neon Telemetry Player plugin to your video players. This will allow us to understand how your video thumbnails are performing on your site, optimize your thumbnails in real time, and provide you with analytics about your thumbnail performance.</p>
