2456f407f1339b8d07a2c555b3cab89394f4fdb7
Anatoli Papirovski <apapirovski@mac.com>
2016-06-24 18:41:34 +0200

    Image overlay vertical, horizontal and square versions

--- src/js/xx/components/ImageZoom.js
+++ src/js/xx/components/ImageZoom.js
@@ -44,14 +44,42 @@ export default class XXPageOverlay extends React.Component {
                 <a href="" className="xxOverlay-close" onClick={handleCloseClick}>Close</a>
                 <div className="xxImageZoom">
                     <div className="xxImageZoom-inner">
+                        {/*
+                            Ideally, the back-end provides image dimensions
+                            paddingBottom, rather than needing to use DOM access
+                            to calculate. You would also use those to determine
+                            whether to set --vertical or --square modifier.
+                        */}
                         {/*<div className="xxImageZoom-image">
                             <XXThumbnail
                                 className="xxThumbnail--zoom"
                                 score={75}
                                 size="large"
-                                src="/img/xx/temporary/thumbnail-3.jpg"
+                                src="/img/xx/temporary/thumbnail-5.jpg"
+                                style={{paddingBottom: `${356 / 636 * 100}%`}}
                             />
                         </div>*/}
+                        {/*<div className="xxImageZoom-image xxImageZoom-image--square">
+                            <XXThumbnail
+                                className="xxThumbnail--zoom"
+                                score={75}
+                                size="large"
+                                src="/img/xx/temporary/thumbnail-4.jpg"
+                                style={{paddingBottom: `${636 / 636 * 100}%`}}
+                            />
+                        </div>*/}
+                        <div
+                            className="xxImageZoom-image xxImageZoom-image--vertical"
+                            style={{maxWidth: `calc((100vh - 242px) / (496 / 370))`}}
+                        >
+                            <XXThumbnail
+                                className="xxThumbnail--zoom"
+                                score={75}
+                                size="large"
+                                src="/img/xx/temporary/thumbnail-3.jpg"
+                                style={{paddingBottom: `${496 / 370 * 100}%`}}
+                            />
+                        </div>
                         <div className="xxImageZoom-content">
                             <h2 className="xxSubtitle xxImageZoom-subtitle">Valence Features</h2>
                             <ul className="xxTagList">
