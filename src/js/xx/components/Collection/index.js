8f0cc74134ab1c0620d0cd43237dfe7b89ceefa0
Anatoli Papirovski <apapirovski@mac.com>
2016-06-28 00:00:48 +0200

    Mobile notification module

--- src/js/xx/components/Collection/index.js
+++ src/js/xx/components/Collection/index.js
@@ -13,6 +13,8 @@ import XXCollectionDelete from './Delete';
 
 import XXCollectionProcessingReady from './ProcessingReady';
 
+import XXCollectionMobileNotification from './MobileNotification';
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default class XXCollection extends React.Component {
@@ -107,6 +109,12 @@ export default class XXCollection extends React.Component {
                         <XXCollectionProcessingReady
                             onClick={() => this.setState({ isProcessingReady: false })}
                         />
+                    ) : null
+                }
+
+                {
+                    this.props.isMobile ? (
+                        <XXCollectionMobileNotification />
                     ) : null
                 }
             </article>
