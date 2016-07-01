414acc9c4106a311c11bb91bd6c9967517d4c135
Alex <alexkarim95@gmail.com>
2016-07-01 16:45:31 -0700

    toggle between account types

--- src/js/components/wonderland/Account.js
+++ src/js/components/wonderland/Account.js
@@ -1,6 +1,7 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import React from 'react';
+import AjaxMixin from '../../mixins/Ajax';
 import T from '../../modules/translation';
 import SESSION from '../../modules/session';
 import UTILS from '../../modules/utils';
@@ -9,13 +10,15 @@ import ChangePasswordForm from '../forms/ChangePasswordForm';
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 var Account = React.createClass({
+    mixins: [AjaxMixin],
     contextTypes: {     
         router: React.PropTypes.object.isRequired     
     },
     getInitialState: function() {
         return {
             displayName: '',
-            username: ''
+            username: '',
+            isPaidUser: false
         }
     },
     componentWillMount: function() {
@@ -31,6 +34,19 @@ var Account = React.createClass({
                 console.error(err);
             })
         ;
+        self.GET('billing/account')
+            .then(function (res) {
+                self.setState({
+                    isPaidUser: true
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+                self.setState({
+                    isPaidUser: false
+                });
+            })
+        ;
     },
     handleLogOut: function(e) {
         var self = this;
@@ -38,18 +54,22 @@ var Account = React.createClass({
         self.context.router.push(UTILS.DRY_NAV.SIGNOUT.URL);
     },
     render: function() {
-        var self = this,
-            accountBodyText = T.get('copy.account.body', {
-                '@link': UTILS.PRICING_EXTERNAL_URL
-            })
-        ;
+        var self = this;
         return (
             <article className="xxPageOverlay-content">
                 <h1 className="xxSubtitle">{T.get('nav.account')}</h1>
                 <h2 className="xxTitle">{T.get('copy.account.heading', {'@displayName': self.state.displayName})}</h2>
-                <div className="xxText">
-                    <p dangerouslySetInnerHTML={{__html: accountBodyText}} />
-                </div>
+                {
+                    (!self.state.isPaidUser) ? (
+                        <div className="xxText">
+                            <p dangerouslySetInnerHTML={{__html: T.get('copy.account.body', {
+                                '@link': UTILS.PRICING_EXTERNAL_URL
+                            })}} />
+                        </div>
+                    ) : (
+                        <div>cash money</div>
+                    )
+                }
                 <div className="xxFormButtons">
                     <button className="xxButton" type="button" onClick={self.handleLogOut}>{T.get('logOut')}</button>
                 </div>
