54271a5a86bf68fb5361642fc45d7c2668eb9b6c
Alex <alexkarim95@gmail.com>
2016-07-11 14:28:51 -0700

    styling

--- src/js/components/pages/TermsPage.js
+++ src/js/components/pages/TermsPage.js
@@ -15,21 +15,14 @@ var TermsPage = React.createClass({
     // mixins: [ReactDebugMixin],
     render: function() {
         return (
-            <div>
+            <main className="xxPage">
                 <Helmet
                     title={UTILS.buildPageTitle(T.get('copy.terms.title'))}
                 />
                 <SiteHeader />
-                <section className="wonderland-section section">
-                    <div className="columns is-desktop">
-                        <div className="column is-half is-offset-one-quarter">
-                            <h1 className="title is-2">{T.get('copy.terms.heading')}</h1>
-                            <TermsOfService />
-                        </div>
-                    </div>
-                </section>
+                <TermsOfService />
                 <SiteFooter />
-            </div>
+            </main>
         );
     }
 });
