284b0088c013fbdd73e9b0ad9aeb8ae7a61ab880
Damian Galarza & Avielle Wolfe <damian+avielle@thoughtbot.com>
2016-07-11 13:53:58 -0400

    Refactor OnboardingTutorial
    * extract TutorialSlide to handle slide specific logic, such as the display of buttons
    * extract TutorialButton to contain button markup

--- src/js/components/wonderland/OnboardingTutorial.js
+++ src/js/components/wonderland/OnboardingTutorial.js
@@ -1,6 +1,7 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import React from 'react';
+import TutorialSlide from './TutorialSlide';
 import T from '../../modules/translation';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -11,6 +12,7 @@ const slides = [
         title: T.get('copy.tutorial.ready.title'),
         description: null,
         image: null,
+        first: true,
     },
     {
         id: 'neonscore',
@@ -29,6 +31,7 @@ const slides = [
         title: T.get('copy.tutorial.upload.title'),
         description: T.get('copy.tutorial.upload.description'),
         image: '/img/tutorial-upload.png',
+        last: true,
     },
 ];
 
@@ -62,64 +65,15 @@ export default class OnboardingTutorial extends React.Component {
             <article className="xxOverlay xxOnboardingTutorial">
                 {
                     slides.map((slide, index) => {
-                        const slideClassName = ['xxFeatureContent', 'xxOnboardingTutorial-slide'];
-                        if (index === activeSlide) {
-                            slideClassName.push('is-active');
-                        }
-
                         return (
-                            <div className={slideClassName.join(' ')} key={slide.id}>
-                                {
-                                    slide.image ? (
-                                        <img
-                                            src={slide.image}
-                                            className="xxFeatureContent-image"
-                                        />
-                                    ) : null
-                                }
-                                <h2 className="xxTitle">{slide.title}</h2>
-                                <p>{slide.description}</p>
-                                <div className="xxFormButtons xxFeatureContent-buttons">
-                                    {
-                                        index !== 0 ? (
-                                            <a
-                                                href=""
-                                                className="xxButton xxButton--transparent"
-                                                onClick={e => setActiveSlide(activeSlide - 1, e)}
-                                            >{T.get('back')}</a>
-                                        ) : null
-                                    }
-                                    {
-                                        index + 1 < slides.length ? (
-                                            <a
-                                                href=""
-                                                className="xxButton xxButton--highlight"
-                                                onClick={e => setActiveSlide(activeSlide + 1, e)}
-                                            >{T.get('action.next')}</a>
-                                        ) : null
-                                    }
-                                    {
-                                        index + 1 >= slides.length ? (
-                                            <a
-                                                href=""
-                                                className="xxButton xxButton--highlight"
-                                                onClick={onClose}
-                                            >{T.get('copy.tutorial.checkItOut')}</a>
-                                        ) : null
-                                    }
-                                </div>
-                                {
-                                    index + 1 < slides.length ? (
-                                        <div className="xxFeatureContent-small">
-                                            <a
-                                                href=""
-                                                className="u-inheritColor"
-                                                onClick={onClose}
-                                            >{T.get('copy.tutorial.skip')}</a>
-                                        </div>
-                                    ) : null
-                                }
-                            </div>
+                            <TutorialSlide
+                                active={activeSlide === index}
+                                key={index}
+                                onBack={e => setActiveSlide(activeSlide - 1, e)}
+                                onClose={onClose}
+                                onNext={e => setActiveSlide(activeSlide + 1, e)}
+                                slide={slide}
+                            />
                         );
                     })
                 }
