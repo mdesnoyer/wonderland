a1fe79532a5c6f7b6f3162f949e32eedb168db17
Alex <alexkarim95@gmail.com>
2016-07-14 00:32:51 -0700

    overview content changes

--- src/js/components/tabs/SupportTab1.js
+++ src/js/components/tabs/SupportTab1.js
@@ -20,19 +20,43 @@ var SupportTab1 = React.createClass({
                 <ol className="simple-ol-list">
                     <li>Log in to your Neon account.</li>
                     <li>Paste a URL that links to a video stored online into the Video URL field.
-                        <figure className="image wonderland-image">
-                            <img src="/img/support/overview/1-video-url.png" />
+                        <figure>
+                            <img src="/img/support/overview/1-analyze-video.png" />
                         </figure>
                     </li>
-                    <li>Click &ldquo;Analyze&rdquo;. You can optionally include a URL to a default video thumbnail that you&rsquo;d like to associate with this video in your Neon account.</li>
-                    <li>Neon&rsquo;s software will analyze your video to surface the most engaging thumbnails. This process takes about the length of the video. You can navigate away from your Neon account and check back later without affecting the process. You can also choose to analyze additional videos during this time.</li>
-                    <li>Your Neon account will show the top thumbnails for each video when your videos have finished processing. You can now download images by clicking on the &ldquo;download image&rdquo; icon next to your image.
-                        <figure className="image wonderland-image">
-                            <img src="/img/support/overview/2-download-image.png" />
+                    <li>Click &ldquo;Submit&rdquo; to begin the video analysis process.</li>
+                    <li>Neon&rsquo;s software will analyze your video to surface the most engaging thumbnails. This process takes about two times the length of the video to complete. You can navigate away from your Neon account and check back later without affecting the process.</li>
+                    <li>Your Neon account will show the top, highest-scoring thumbnails for each video when your videos have finished processing. Your account will also show the low-scoring thumbnails for each video. NeonScore corresponds with valence—the higher the score, the more likely the image is to be clicked. An image&rsquo;s NeonScore is the number in the orange box in the lower left corner.
+                        <figure>
+                            <img src="/img/support/overview/2-high-and-low-scoring-images.png" />
                         </figure>
-                        You can also copy the static link to your images by clicking on the &ldquo;copy image URL&rdquo; icon next to your image in your Neon account.
-                        <figure className="image wonderland-image">
-                            <img src="/img/support/overview/3-copy-image-url.png" />
+                        By hovering over images in the image collection view, you can see the projected lift for that image compared to the default image. Lift is the increase in click-through rate for one image over another image, in this case the Neon-selected image vs the default image for the video.
+                        <figure>
+                            <img src="/img/support/overview/3-lift-hover.png" />
+                        </figure>
+                        By clicking on a Neon-selected image, you can see the automatically detected valence features that contributed to the perceived engagingness and the NeonScore for the image.
+                        <figure>
+                            <img src="/img/support/overview/4-valence-features.png" />
+                        </figure>
+                        If you’d like to see the images from your video that are most suited to a particular audience, you can reprocess the video for a demographic. To do this, click on the orange &ldquo;filter&rdquo; button.
+                        <figure>
+                            <img src="/img/support/overview/5-refilter-hover.png" />
+                        </figure>
+                        Then, select a target gender and/or age group, and click &ldquo;Apply&rdquo;.
+                        <figure>
+                            <img src="/img/support/overview/6-refilter-apply.png" />
+                        </figure>
+                        Because our software will need to search through your video to find the best audience-specific thumbnails, you will be able to view your new thumbnails after the video is reprocessed. To look at thumbnails for different audiences you’ve processed for in the past, click the down arrow next to &ldquo;Filters&rdquo; and select the audience you&rsquo;d like to see images for.
+                        <figure>
+                            <img src="/img/support/overview/7-view-other-demographics.png" />
+                        </figure>
+                        You can share your image results on social media by clicking on the share icon.
+                        <figure>
+                            <img src="/img/support/overview/8-share-hover.png" />
+                        </figure>
+                        You can also receive an email containing the image results for a specific video by clicking on the email icon.
+                        <figure>
+                            <img src="/img/support/overview/9-email-hover.png" />
                         </figure>
                     </li>
                 </ol>
@@ -44,24 +68,19 @@ var SupportTab1 = React.createClass({
                 <p>We analyze your videos to extract and surface the best images to use as video thumbnails. We&rsquo;re able to analyze videos that are hosted on the internet and are publicly accessible. You can submit a video in two ways: via our UI, or via the CMS API. Once you&rsquo;ve submitted video content, our software analyzes your video, identifies engaging frames using our deep neural net-based model of the human visual system, and surfaces the images most likely to engage your users. This process takes approximately the same amount of time as the length of the video being processed.</p>
                 
                 <h1 className="xxPage-Subtitle">Editorial Partnering</h1>
-                <p>Once your content has been analyzed, your Neon account will be populated with the top most effective images from your video. While our models are highly adept at understanding and predicting a user&rsquo;s gut reaction to an image, they&rsquo;re not quite as good as humans at understanding image context. This is why we give you the ability to disable some or all of a video&rsquo;s Neon images or add other images in your Neon Account. As with most complex systems, ours works best as a partnership between human and machine.</p>
-                <p>If you have integrated your Brightcove account or other image platform, you can indicate which images you would like to exclude from image serving in your Neon account. Simply click on the image you&rsquo;d like to disable, then click the X on the left sidebar on the image.</p>
-                <figure className="image wonderland-image">
-                    <img src="/img/support/overview/4-how-to-disable.png" />
+                <p>Once your content has been analyzed, your Neon account will be populated with the top most effective images from your video. While our models are highly adept at understanding and predicting a user&rsquo;s gut reaction to an image, they&rsquo;re not quite as good as humans at understanding image context. This is why we give you the ability manually manage your video thumbnails by hiding a video from view in your Neon account. As with most complex systems, ours works best as a partnership between human and machine.</p>
+                <p>You can hide a video from your Neon account by clicking the &ldquo;delete&rdquo; icon and then confirming that you&rsquo;d like to delete your video.</p>
+                <figure>
+                    <img src="/img/support/overview/10-hover-delete.png" />
                 </figure>
-                <p>A disabled image will have an X over the image.</p>
-                <figure className="image wonderland-image">
-                    <img src="/img/support/overview/5-disabled.png" />
+                <figure>
+                    <img src="/img/support/overview/11-delete-collection.png" />
                 </figure>
-                <p>If you&rsquo;d like to re-enable a disabled image, simply click on the checkmark on the left sidebar of the image. The orange X over the image should disappear, and the image will now be served alongside your other enabled images for that video.</p>
+                <p>This will cause the video to be hidden from view in your Neon account. Please note that this will not cause the deleted video to disappear from your CMS or OVP.</p>
 
                 <h1 className="xxPage-Subtitle">Automated A/B Testing, Optimization, and Realtime Analytics</h1>
                 <p>Though our models are very good at predicting the best image for your content, let&rsquo;s be honest: people are complex. They will always surprise us. So, to ensure that we are always showing the best image to your users, we run an A/B test with the set of images that represent your content. Once we are statistically confident that the best image has been found, we then make sure that we show only that image to the rest of your users, thus creating the most value possible for you.</p>
                 <p>Running the A/B test provides a secondary valuable insight: being able to measure how effective each image is. This gives you some insight into how your users interact with images on your site, and additionally allows our models to constantly learn and improve.</p>
-                <p>You can see this data in realtime by clicking on any image in your Neon account that is currently or has previously been live on your site. The clickthrough rate (CTR) will be listed on the right side of the image panel.</p>
-                <figure className="image wonderland-image">
-                    <img src="/img/support/overview/6-ctr.png" />
-                </figure>
                 <p>To perform the A/B testing, we serve images directly to your users and then collect <a href={UTILS.DRY_NAV.TELEMETRY.URL}>telemetry</a>, or data from your site, about how they interact with those images. Enabling this requires two steps:</p>
                 <ol className="simple-ol-list">
                     <li>Inserting our serving URLs onto your site in place of the images currently there. These serving URLs point to our image serving platform, which determines which image each user will see. It will then issue a 302 redirect to the actual image sitting on a CDN.</li>
