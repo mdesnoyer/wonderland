f031620e05a4b3681cc221018ff126e79483816a
Alex Karim <alexkarim95@gmail.com>
2016-07-20 11:27:54 -0700

    Mobile Results Page #1414 (#525)

--- src/js/components/wonderland/VideosMobileWarning.js
+++ src/js/components/wonderland/VideosMobileWarning.js
@@ -4,28 +4,9 @@ import T from '../../modules/translation';
 export default class VideosMobileWarning extends React.Component {
     constructor(props) {
         super(props);
-
-        this.updateField = this.updateField.bind(this);
-
-        this.state = {
-            email: '',
-        };
-    }
-
-    updateField(field, value) {
-        this.setState({
-            [field]: value,
-        });
     }
 
     render() {
-        const isValid = this.state.email;
-
-        const sendClassName = ['xxButton', 'xxButton--highlight'];
-        if (isValid) {
-            sendClassName.push('xxButton--important');
-        }
-
         return (
             <div className="xxCollectionMobileNotification">
                 <h2 className="xxCollection-title">
@@ -34,23 +15,6 @@ export default class VideosMobileWarning extends React.Component {
                 <p className="xxCollectionMobileNotification-text">
                     {T.get('copy.mobile.warning.description')}
                 </p>
-                <div className="xxFormField">
-                    <label className="xxLabel">{T.get('label.yourEmail')}</label>
-                    <input
-                        className="xxInputText"
-                        type="text"
-                        placeholder="example@email.com"
-                        value={this.state.email}
-                        onChange={e => this.updateField('email', e.target.value)}
-                    />
-                </div>
-                <div className="xxFormButtons">
-                    <button
-                        disabled={!isValid}
-                        className={sendClassName.join(' ')}
-                        type="button"
-                    >{T.get('send')}</button>
-                </div>
             </div>
         );
     }
