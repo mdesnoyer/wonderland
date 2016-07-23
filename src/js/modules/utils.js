a5f9668b5156ae19e4033540f8af726f8fa1d149
scaulfield01 <caulfield@neon-lab.com>
2016-07-22 20:12:01 -0700

    15 to 900

--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -317,7 +317,7 @@ var UTILS = {
     PRICING_EXTERNAL_URL: 'https://neon-lab.com/pricing/',
     VIDEO_CHECK_INTERVAL_BASE: 10000, // 10s
     RESULTS_PAGE_SIZE: 5,
-    MAX_VIDEO_SIZE: 15,
+    MAX_VIDEO_SIZE: 900,
     VIDEO_FIELDS: ['video_id', 'title', 'publish_date', 'created', 'updated', 'duration', 'state', 'url', 'thumbnails', 'demographic_thumbnails', 'bad_thumbnails', 'estimated_time_remaining'],
     THUMBNAIL_FIELDS: ['thumbnail_id'],
     VIDEO_STATS_FIELDS: ['experiment_state', 'winner_thumbnail', 'created', 'updated'],
