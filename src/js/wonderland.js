41ba5f91dbee69a1832e1815bb442307a017158b
Nate Kresge <kresge@neon-lab.com>
2016-07-27 15:57:09 -0700

    New route for direct vsp re

--- src/js/wonderland.js
+++ src/js/wonderland.js
@@ -80,6 +80,7 @@ render((
         <Route path="/video/:videoId/" component={VideoPageOwner} />
 
         <Route path="/share/video/:videoId/account/:accountId/token/:shareToken/(index.html)" component={VideoPageGuest} />
+        <Route path="/share_direct/video/:videoId/account/:accountId/token/:shareToken/(index.html)" component={VideoPageGuest} />
 
         <Route path={UTILS.DRY_NAV.TERMS.URL} component={TermsPage} />
         {/* <Route path={UTILS.DRY_NAV.BILLING.URL} component={BillingPage} /> */}
