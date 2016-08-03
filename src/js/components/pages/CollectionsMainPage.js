3bb07c5798f353131d21bb9ee97670279cca7561
scaulfield01 <caulfield@neon-lab.com>
2016-08-03 12:14:36 -0700

    batch set up need help on back end to gather all images

--- src/js/components/pages/CollectionsMainPage.js
+++ src/js/components/pages/CollectionsMainPage.js
@@ -6,6 +6,7 @@ import AjaxMixin from '../../mixins/Ajax';
 
 import UTILS from '../../modules/utils';
 import SESSION from '../../modules/session';
+import { objectToGetParams } from '../../modules/sharing';
 
 import CollectionsContainer from '../knave/CollectionsContainer';
 import SiteHeader from '../wonderland/SiteHeader';
@@ -75,83 +76,55 @@ var CollectionsMainPage = React.createClass({
         e.target.dataset.type === 'image' && self.postImages(self.state.imageUrl);
     },
     postVideo: function(url) {
-        var self = this,
-            videoId = UTILS.generateId(),
-            options = {
-                data: {
-                    external_video_ref: videoId,
-                    url: UTILS.properEncodeURI(UTILS.dropboxUrlFilter(url))
-                }
-            }
-        ;
-            self.POST('videos', options)
-                .then(function(json) {
-                    debugger 
-                    self.updateCollectionVideo(json.video.video_id)
-                })
-                .catch(function(err) {
-                    debugger
-                });
+        alert('vidoes!')
     },
     postImages: function() {
         alert('images!')
     },
-    getAccountLimits: function(callback) {
-        var self = this;
-        self.GET('limits')
-            .then(function(res) {
-                //keep next page link + limit
-                // {max_video_size: 900, max_video_posts: 25, refresh_time_video_posts: "2016-08-02 22:34:01.854838", video_posts: 0}
-                callback()
-            })
-            .catch(function(err) {
-                // if there is an erro we should we 
-                //more than likely need to sign in
-            });
-    },
     getCollections: function(paging) {
         var self = this,
-            options = {data: { limit: UTILS.RESULTS_PAGE_SIZE }}
+            options = {data: { limit: UTILS.RESULTS_PAGE_SIZE , tag_type: 'col' }}
         // paging = paging ? paging.split('?')[1] : ''
         // grab the keys of the first 5 collections
         //refresh the token first ? 
         self.GET('tags/search?', options)
             .then(function(res) {
-                debugger
                 //set next page to the state
                 self.setState({ nextPage: res.next_page});
                 //create a request array for the batch for tags
-                var requestTag = {data: { requests: self.createRequests(res, 'tag', 'POST') }}
+                var requestTag = {
+                    data: {
+                        call_info: {
+                            refresh_token: SESSION.state.refreshToken,
+                            access_token: SESSION.state.accessToken,
+                            requests: self.createRequests(res, 'tags', 'GET')
+                        }
+                    },
+                    noAccountId: true
+                }
                 debugger
-                // self.GET('tag', {data: requestTag.data.requests[0].body})
-                // .then(function(res){
-                //     debugger
-                // })
-                // .catch(function(err){
-                //     debugger
-                // })
-                // zmwpqq7dfkubotxgb03j3s80
-                // self.POST('batch/', requestTag)
-                //     .then(function(res) {
-                //         debugger
-                //         //create a request array fro the batch of thumbnails
-                //         // var requestThumbs = self.createRequests(res, 'tag', 'POST')
-                //         // self.POST('batch', requestThumbs)
-                //         //     .then(function(res) {
-                //         //         // grab response and then set it to state
+                self.POST('batch', requestTag)
+                    .then(function(res) {
+                        debugger
+                        //create a request array fro the batch of thumbnails
+                        var requestThumbs = self.createRequests(res, 'tag', 'POST')
+                        self.POST('batch', requestThumbs)
+                            .then(function(res) {
+                                // grab response and then set it to state
 
-                //         //         self.setState({
-                //         //             collections: res.results
-                //         //         })
-                //         //     })
-                //         //     .catch(function(err) {
-                //         //     })
-                //     })
-                //     .catch(function(err){
-                //         debugger
-                //     })
+                                self.setState({
+                                    collections: res.results
+                                })
+                            })
+                            .catch(function(err) {
+                            })
+                    })
+                    .catch(function(err){
+                        debugger
+                    })
             })
             .catch(function(err) {
+                debugger
             })
     },
 
@@ -159,19 +132,17 @@ var CollectionsMainPage = React.createClass({
         var self = this,
         //what if account ID not set ? 
             accountId = SESSION.state.accountId,
-            // batch = {
-
-            // },
             requests = [],
             responseArrayName,
             responsePropName,
             responseIdName
         ;
+        // need to hanlde posts and their data
         switch(type) {
-            case 'tag': 
+            case 'tags': 
                 responseArrayName = 'items';
                 responseIdName = 'tag_id';
-                responsePropName = 'key'
+                responsePropName = 'key';
                 break;
             case 'thumbnails':
                 responseArrayName = 'requests';
@@ -180,16 +151,24 @@ var CollectionsMainPage = React.createClass({
             default: 
                 responseArrayName = 'requests'; 
         }
-        res[responseArrayName].map(function(item) {
-            requests.push({
-                body: { 'key': item[responsePropName]},
-                method: method,
-                relative_url: '/api/v2/' + accountId + '/' + type + '/'
-            })
-        })
-        // debugger
-        // batch.requests = requests;
-        // debugger
+        if (method === 'GET') {
+            res[responseArrayName].map(function(item) {
+                requests.push({
+                    method: method,
+                    relative_url: '/api/v2/' + accountId + '/' + type + '/?' + responseIdName + '=' + item[responsePropName]
+                })
+            })            
+        }
+        else {
+            res[responseArrayName].map(function(item) {
+                requests.push({
+                    body: { [responseIdName]: item[responsePropName]},
+                    method: method,
+                    relative_url: '/api/v2/' + accountId + '/' + type + '/'
+                })
+            })  
+        }
+
         return requests
     }
 });
@@ -250,3 +229,36 @@ export default CollectionsMainPage;
         //         }
         //     }
         // ;
+        // postVideo: function(url) {
+        //     var self = this,
+        //         videoId = UTILS.generateId(),
+        //         options = {
+        //             data: {
+        //                 external_video_ref: videoId,
+        //                 url: UTILS.properEncodeURI(UTILS.dropboxUrlFilter(url))
+        //             }
+        //         }
+        //     ;
+        //         self.POST('videos', options)
+        //             .then(function(json) {
+        //                 debugger 
+        //                 self.updateCollectionVideo(json.video.video_id)
+        //             })
+        //             .catch(function(err) {
+        //                 debugger
+        //             });
+        // },
+        // getAccountLimits: function(callback) {
+        //     var self = this;
+        //     self.GET('limits')
+        //         .then(function(res) {
+        //             //keep next page link + limit
+        //             // {max_video_size: 900, max_video_posts: 25, refresh_time_video_posts: "2016-08-02 22:34:01.854838", video_posts: 0}
+        //             callback()
+        //         })
+        //         .catch(function(err) {
+        //             // if there is an erro we should we 
+        //             //more than likely need to sign in
+        //         });
+        // },
+
