a1ccb2b022699efa5d2ffec8287a87f33e3fff51
Alex <alexkarim95@gmail.com>
2016-08-08 13:59:40 -0700

    skeleton

--- src/js/components/pages/SignInPage.js
+++ src/js/components/pages/SignInPage.js
@@ -12,9 +12,16 @@ import SESSION from '../../modules/session';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+const BREAKPOINT_MOBILE = 768;
+
 var SignInPage = React.createClass({
     contextTypes: {
         router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function () {
+        return {
+            windowWidth: window.outerWidth
+        };
     },
     componentWillMount: function() {
         var self = this;
@@ -22,17 +29,36 @@ var SignInPage = React.createClass({
             self.context.router.push(UTILS.DRY_NAV.DASHBOARD.URL);
         }
     },
+    componentDidMount: function() {
+        window.addEventListener('resize', this.handleWindowResize);
+        this.handleWindowResize();
+    },
+    handleWindowResize: function() {
+        const windowWidth = window.outerWidth;
+        if (this.state.windowWidth !== windowWidth) {
+            this.setState({
+                windowWidth,
+            });
+        }
+        if (windowWidth < BREAKPOINT_MOBILE) {
+            document.documentElement.classList.add('is-mobile');
+        } else {
+            document.documentElement.classList.remove('is-mobile');
+        }
+    },
     render: function() {
         var self = this;
+        const isMobile = self.state.windowWidth < BREAKPOINT_MOBILE;
         return (
             <main className="xxPage">
                 <Helmet
                     title={UTILS.buildPageTitle(T.get('copy.signIn.title'))}
+                    meta={[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"},]}
                 />
                 <SiteHeader />
                 <section className="xxMainForm">
                     <h1 className="xxTitle">{T.get('action.signIn')}</h1>
-                    <SignInForm />
+                    <SignInForm isMobile={isMobile} />
                 </section>
                 <SiteFooter />
             </main>
