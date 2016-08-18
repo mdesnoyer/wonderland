4b0612d016b80447134dc5c32796b9af352836c4
Nate Kresge <kresge@neon-lab.com>
2016-08-18 14:57:08 -0700

    Extract stores from container to page parent

--- src/js/components/knave/CollectionsContainer.js
+++ src/js/components/knave/CollectionsContainer.js
@@ -4,7 +4,6 @@ import React, {PropTypes} from 'react';
 
 import _ from 'lodash';
 
-import AjaxMixin from '../../mixins/Ajax';
 import UTILS from '../../modules/utils';
 
 import ImageCollection from './ImageCollection';
@@ -28,64 +27,43 @@ import {
 
 const CollectionsContainer = React.createClass({
 
-    mixins: [AjaxMixin],
-
-    componentWillMount() {
-        this.search();
+    propTypes: {
+        loadThumbnails: PropTypes.func.isRequired,
+        numberToShow: PropTypes.number.isRequired,
+        // TODO shape this
+        // Map of store identifying key to the store,
+        // which is a map of object id to object.
+        stores: PropTypes.object.isRequired
     },
 
     getInitialState: function() {
         return {
 
-            // These are stores of tag, thumb and video resources.
-            // Map of id to tag.
-            // @TODO factor to store.
-            tags: {},
+            // List of tag ids to render.
+            shownIds: [],
 
             // Map of tag id to integer index of gender, then age.
             // Uses FILTER_GENDER_COL_ENUM, FILTER_AGE_COL_ENUM.
             // By default, the demographic is gender=none, age=none.
-            selectedDemographic: {},
-
-            // Map of gender, age, thumbnail id to thumbnail.
-            // @TODO consider how to initialize this structure.
-            thumbnails: {
-                0: {
-                    0: {},
-                    1: {},
-                    2: {},
-                    3: {},
-                    4: {},
-                    5: {}
-                },
-                1: {
-                    0: {},
-                    1: {},
-                    2: {},
-                    3: {},
-                    4: {},
-                    5: {}
-                },
-                2: {
-                    0: {},
-                    1: {},
-                    2: {},
-                    3: {},
-                    4: {},
-                    5: {}
-                }
-            },
-
-            // Map of id to video.
-            videos: {},
+            selectedDemographic: {}
         };
+    },
+
+    // Tie into the parent's updating of its stores.
+    componentWillReceiveProps: function(nextProps) {
+        // TODO work on paging, sorting behavior.
+        const shownIds = _.values(nextProps.stores.tags)
+            .slice(0, nextProps.numberToShow)
+            .map(t => {
+                return t.tag_id;
+            });
+        this.setState({shownIds: shownIds});
     },
 
     // Return array of gender,age enum array based
     // on state of collection.
     getDemoOptionArray: function(tagId) {
-
-        const tag = this.state.tags[tagId];
+        const tag = this.props.stores.tags[tagId];
         switch(tag.tag_type) {
         case UTILS.TAG_TYPE_IMAGE_COL:
             // All factor posibilities are available.
@@ -95,7 +73,7 @@ const CollectionsContainer = React.createClass({
                 _.values(UTILS.FILTER_AGE_COL_ENUM)
             );
         case UTILS.TAG_TYPE_VIDEO_COL:
-            const video = this.state.videos[tag.video_id];
+            const video = this.props.stores.videos[tag.video_id];
             return video.demographic_thumbnails.map(demo => {
                 return [
                     UTILS.FILTER_GENDER_COL_ENUM[demo.gender],
@@ -112,7 +90,7 @@ const CollectionsContainer = React.createClass({
     // Else return an error component.
     buildCollectionComponent: function(tagId) {
 
-        const collection = this.state.tags[tagId];
+        const collection = this.props.stores.tags[tagId];
 
         // If a demographic was selected, use it. Else nulls.
         let gender,
@@ -133,8 +111,8 @@ const CollectionsContainer = React.createClass({
             const missingThumbIds = [];
             const [gender, age] = demoKey;
 
-            this.state.tags[tagId].thumbnail_ids.map(tid => {
-                if (undefined === this.state.thumbnails[gender][age][tid]) {
+            this.props.stores.tags[tagId].thumbnail_ids.map(tid => {
+                if (undefined === this.props.stores.thumbnails[gender][age][tid]) {
                     missingThumbIds.push(tid);
                 }
             });
@@ -144,7 +122,8 @@ const CollectionsContainer = React.createClass({
             };
             if (missingThumbIds.length > 0) {
                 // Load thumbnails for image-type collection.
-                return this.loadThumbnails(missingThumbIds, gender, age, stateDiff);
+                // TODO? deal with async here
+                this.props.loadThumbnails(missingThumbIds, gender, age, stateDiff);
             }
             // Else, just change the selected demographic for the tag.
             this.setState(stateDiff);
@@ -164,7 +143,7 @@ const CollectionsContainer = React.createClass({
     buildImageCollectionComponent: function(tagId, collection, onDemoChange, gender, age) {
 
         const allThumbnailMap = _.pick(
-            this.state.thumbnails[gender][age],
+            this.props.stores.thumbnails[gender][age],
             collection.thumbnail_ids)
         const right = UTILS.bestThumbnail(_.values(allThumbnailMap));
         const left = UTILS.worstThumbnail(_.values(allThumbnailMap));
@@ -215,7 +194,7 @@ const CollectionsContainer = React.createClass({
     // @TODO factor common code
     buildVideoCollectionComponent(tagId, collection, onDemoChange, gender, age) {
 
-        const video = this.state.videos[collection.video_id];
+        const video = this.props.stores.videos[collection.video_id];
         let genderLabel = _.invert(UTILS.FILTER_GENDER_COL_ENUM)[gender];
         if(genderLabel == 'null') {
             genderLabel = null;
@@ -232,7 +211,7 @@ const CollectionsContainer = React.createClass({
         );
 
         const allThumbnailMap = _.pick(
-            this.state.thumbnails[gender][age],
+            this.props.stores.thumbnails[gender][age],
             videoDemo.thumbnails.map(t => {
                 return t.thumbnail_id;
             }));
@@ -256,7 +235,7 @@ const CollectionsContainer = React.createClass({
 
         // Do the same for the bad thumbnail list.
         const allBadThumbnailMap = _.pick(
-            this.state.thumbnails[gender][age],
+            this.props.stores.thumbnails[gender][age],
             videoDemo.bad_thumbnails.map(t => {
                 return t.thumbnail_id;
             })
@@ -300,145 +279,8 @@ const CollectionsContainer = React.createClass({
             />
        );
     },
-
-    search: function() {
-        const self = this,
-            options = {
-                data: {
-                    limit: UTILS.RESULTS_PAGE_SIZE}};
-
-        const state = self.getInitialState();
-        const pageQueryParam = '?limit=' + this.props.numberToDisplay;
-
-        // Search for tag ids, get tags and videos, then get thumbnails for those.
-        self.GET('tags/search', options)
-        .then(searchRes => {
-
-            // Search for all the tags.
-            const _tagData = {
-                tag_id: searchRes.items.reduce((tag_ids, tag) => {
-                    tag_ids.push(tag.tag_id);
-                    return tag_ids;
-                }, []).join(',')
-            };
-            const tagsPromise = self.GET('tags', {data: _tagData});
-
-            // Additionally search on videos in the result.
-            const _videoData = {
-                video_id: searchRes.items.reduce((video_ids, tag) => {
-                    if(tag.video_id) {
-                        video_ids.push(tag.video_id);
-                    }
-                    return video_ids;
-                }, []).join(','),
-
-                fields: UTILS.VIDEO_FIELDS.join(',')
-            };
-            const videosPromise = _videoData.video_id?
-                self.GET('videos', {data: _videoData}):
-                null;
-
-            return Promise.all([tagsPromise, videosPromise]);
-        })
-        .then(combined => {
-
-            // Unpack promises.
-            let tagsRes,
-                videosRes;
-            tagsRes = combined[0] || {};
-            videosRes = combined[1] || {videos: []};
-
-            // Store the map of collections, videos.
-            state.tags = tagsRes;
-            videosRes.videos.map(video => {
-                state.videos[video.video_id] = video;
-            });;
-
-            // Store the demographic thumbnails.
-            _.values(state.videos).map(video => {
-                video.demographic_thumbnails.map(dem => {
-                    let gender = UTILS.FILTER_GENDER_COL_ENUM[dem.gender];
-                    let age = UTILS.FILTER_AGE_COL_ENUM[dem.age];
-                    if (age === undefined || gender === undefined) {
-                        console.warn('Unknown demo ', dem.age, dem.gender);
-                        return;
-                    }
-                    // Set each of the thumbnails and bad thumbnails to store.
-                    dem.thumbnails.map(t => {
-                        state.thumbnails[gender][age][t.thumbnail_id] = t;
-                    });
-                    dem.bad_thumbnails.map(t => {
-                        state.thumbnails[gender][age][t.thumbnail_id] = t;
-                    });
-                });
-            });
-
-            // Get and concatenate all thumbnail ids.
-            const tags = _.values(tagsRes);
-            const thumbIds = tags.reduce((array, tag) => {
-                // Video thumbnails are already stored.
-                if(tag.tag_type !== UTILS.TAG_TYPE_VIDEO_COL) {
-                    array = array.concat(tag.thumbnail_ids);
-                }
-                return array;
-            }, []);
-
-            return this.loadThumbnails(thumbIds, 0, 0, state);
-        })
-    },
-
-    loadThumbnails: function(thumbnailIds, gender=0, age=0, state={}) {
-        const self = this;
-
-        // Empty array of ids is no op; just set passed-in state.
-        if(thumbnailIds.length == 0) {
-            return self.setState(state);
-        }
-
-        // Create array of CSVs of max length.
-        const thumbArgs = UTILS.csvFromArray(thumbnailIds, UTILS.MAX_CSV_VALUE_COUNT);
-        const baseParams = {};
-
-        // If demo parameters are valued and not "null", then include them.
-        if (gender !== 0) {
-            baseParams.gender = _.invert(UTILS.FILTER_GENDER_COL_ENUM)[gender];
-        }
-        if (age !== 0) {
-            baseParams.age = _.invert(UTILS.FILTER_AGE_COL_ENUM)[age];
-        }
-
-        let promise,
-            params;
-        // Batch only large requests since batch is slower.
-        if (thumbArgs.length > 1) {
-            thumbArgs.map(arg => {
-                // Build this batch's params by copying base params and adding the tid arg.
-                params = {};
-                Object.assign(params, baseParams, {thumbnail_id: arg});
-                self.batch('GET', 'thumbnails', params);
-            });
-            promise = self.sendBatch();
-        } else {
-            params = {};
-            Object.assign(params, baseParams, {thumbnail_id: thumbArgs[0]});
-            promise = self.GET('thumbnails', {data: params});
-        }
-
-        promise.then(thumbsRes => {
-            // Merge loaded thumbnails to state.thumbnails.
-            const thumbnails = state.thumbnails? state.thumbnails: self.state.thumbnails;
-            thumbsRes.thumbnails.map(t => {
-                thumbnails[gender][age][t.thumbnail_id] = t;
-            });
-
-            Object.assign(state, {thumbnails: thumbnails});
-            self.setState(state);
-        })
-    },
-
-
     render: function() {
-        const collections = _.keys(this.state.tags).map(tagId => {
+        const collections = this.state.shownIds.map(tagId => {
             return this.buildCollectionComponent(tagId);
         });
         return (<ul>{collections}</ul>);
--- src/js/components/pages/CollectionsMainPage.js
+++ src/js/components/pages/CollectionsMainPage.js
@@ -1,6 +1,9 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import React, {PropTypes} from 'react';
 
+import _ from 'lodash';
+
+import AjaxMixin from '../../mixins/Ajax';
 import UTILS from '../../modules/utils';
 import SESSION from '../../modules/session';
 
@@ -8,15 +11,61 @@ import SiteHeader from '../wonderland/SiteHeader';
 import CollectionsContainer from '../knave/CollectionsContainer';
 import SiteFooter from '../wonderland/SiteFooter';
 
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 const CollectionsMainPage = React.createClass({
+
+    mixins: [AjaxMixin],
+
     contextTypes: {
         router: PropTypes.object.isRequired
     },
 
     getInitialState: function() {
+
         return {
+
+            // These are stores of tag, thumb and video resources.
+            // Map of id to tag.
+            // @TODO factor to store.
+            tags: {},
+
+            // Map of tag id to integer index of gender, then age.
+            // Uses FILTER_GENDER_COL_ENUM, FILTER_AGE_COL_ENUM.
+            // By default, the demographic is gender=none, age=none.
+            selectedDemographic: {},
+
+            // Map of gender, age, thumbnail id to thumbnail.
+            // @TODO consider how to initialize this structure.
+            thumbnails: {
+                0: {
+                    0: {},
+                    1: {},
+                    2: {},
+                    3: {},
+                    4: {},
+                    5: {}
+                },
+                1: {
+                    0: {},
+                    1: {},
+                    2: {},
+                    3: {},
+                    4: {},
+                    5: {}
+                },
+                2: {
+                    0: {},
+                    1: {},
+                    2: {},
+                    3: {},
+                    4: {},
+                    5: {}
+                }
+            },
+
+            // Map of id to video.
+            videos: {},
+
             // State of search paging: current page, page count,
             // next, prev page url.
             search: {
@@ -30,8 +79,145 @@ const CollectionsMainPage = React.createClass({
 
     componentWillMount: function() {
         if (!SESSION.active()) {
-            this.context.router.push(UTILS.DRY_NAV.SIGNIN.URL)
+            return this.context.router.push(UTILS.DRY_NAV.SIGNIN.URL)
         }
+        this.search();
+    },
+
+    // TODO extract to search module
+    search: function() {
+        const self = this,
+            options = {
+                data: {
+                    limit: UTILS.RESULTS_PAGE_SIZE}};
+
+        const state = self.getInitialState();
+        const pageQueryParam = '?limit=' + this.props.numberToDisplay;
+
+        // Search for tag ids, get tags and videos, then get thumbnails for those.
+        self.GET('tags/search', options)
+        .then(searchRes => {
+
+            // Search for all the tags.
+            const _tagData = {
+                tag_id: searchRes.items.reduce((tag_ids, tag) => {
+                    tag_ids.push(tag.tag_id);
+                    return tag_ids;
+                }, []).join(',')
+            };
+            const tagsPromise = self.GET('tags', {data: _tagData});
+
+            // Additionally search on videos in the result.
+            const _videoData = {
+                video_id: searchRes.items.reduce((video_ids, tag) => {
+                    if(tag.video_id) {
+                        video_ids.push(tag.video_id);
+                    }
+                    return video_ids;
+                }, []).join(','),
+
+                fields: UTILS.VIDEO_FIELDS.join(',')
+            };
+            const videosPromise = _videoData.video_id?
+                self.GET('videos', {data: _videoData}):
+                null;
+
+            return Promise.all([tagsPromise, videosPromise]);
+        })
+        .then(combined => {
+
+            // Unpack promises.
+            let tagsRes,
+                videosRes;
+            tagsRes = combined[0] || {};
+            videosRes = combined[1] || {videos: []};
+
+            // Store the map of collections, videos.
+            state.tags = tagsRes;
+            videosRes.videos.map(video => {
+                state.videos[video.video_id] = video;
+            });;
+
+            // Store the demographic thumbnails.
+            _.values(state.videos).map(video => {
+                video.demographic_thumbnails.map(dem => {
+                    let gender = UTILS.FILTER_GENDER_COL_ENUM[dem.gender];
+                    let age = UTILS.FILTER_AGE_COL_ENUM[dem.age];
+                    if (age === undefined || gender === undefined) {
+                        console.warn('Unknown demo ', dem.age, dem.gender);
+                        return;
+                    }
+                    // Set each of the thumbnails and bad thumbnails to store.
+                    dem.thumbnails.map(t => {
+                        state.thumbnails[gender][age][t.thumbnail_id] = t;
+                    });
+                    dem.bad_thumbnails.map(t => {
+                        state.thumbnails[gender][age][t.thumbnail_id] = t;
+                    });
+                });
+            });
+
+            // Get and concatenate all thumbnail ids.
+            const tags = _.values(tagsRes);
+            const thumbIds = tags.reduce((array, tag) => {
+                // Video thumbnails are already stored.
+                if(tag.tag_type !== UTILS.TAG_TYPE_VIDEO_COL) {
+                    array = array.concat(tag.thumbnail_ids);
+                }
+                return array;
+            }, []);
+
+            return this.loadThumbnails(thumbIds, 0, 0, state);
+        })
+    },
+
+    loadThumbnails: function(thumbnailIds, gender=0, age=0, state={}) {
+        const self = this;
+
+        // Empty array of ids is no op; just set passed-in state.
+        if(thumbnailIds.length == 0) {
+            return self.setState(state);
+        }
+
+        // Create array of CSVs of max length.
+        const thumbArgs = UTILS.csvFromArray(thumbnailIds, UTILS.MAX_CSV_VALUE_COUNT);
+        const baseParams = {};
+
+        // If demo parameters are valued and not "null", then include them.
+        if (gender !== 0) {
+            baseParams.gender = _.invert(UTILS.FILTER_GENDER_COL_ENUM)[gender];
+        }
+        if (age !== 0) {
+            baseParams.age = _.invert(UTILS.FILTER_AGE_COL_ENUM)[age];
+        }
+
+        let promise,
+            params;
+        // Batch only large requests since batch is slower.
+        if (thumbArgs.length > 1) {
+            thumbArgs.map(arg => {
+                // Build this batch's params by copying base params and adding the tid arg.
+                params = {};
+                Object.assign(params, baseParams, {thumbnail_id: arg});
+                self.batch('GET', 'thumbnails', params);
+            });
+            promise = self.sendBatch();
+        } else {
+            params = {};
+            Object.assign(params, baseParams, {thumbnail_id: thumbArgs[0]});
+            promise = self.GET('thumbnails', {data: params});
+        }
+
+        promise.then(thumbsRes => {
+            // Merge loaded thumbnails to state.thumbnails.
+            const thumbnails = state.thumbnails? state.thumbnails: self.state.thumbnails;
+            thumbsRes.thumbnails.map(t => {
+                thumbnails[gender][age][t.thumbnail_id] = t;
+            });
+
+            Object.assign(state, {thumbnails: thumbnails});
+            self.setState(state);
+        })
     },
 
     // TODO add post forms.
@@ -39,7 +225,15 @@ const CollectionsMainPage = React.createClass({
         return (
             <main className='xxPage'>
                 <SiteHeader />
-                <CollectionsContainer />
+                <CollectionsContainer
+                    numberToShow={UTILS.RESULTS_PAGE_SIZE}
+                    stores={{
+                        tags: this.state.tags,
+                        thumbnails: this.state.thumbnails,
+                        videos: this.state.videos
+                    }}
+                    loadThumbnails={this.loadThumbnails}
+                />
                 <SiteFooter />
             </main>
         );
