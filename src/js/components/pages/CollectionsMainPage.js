5202c50239229b9e2615feebb0bc7b850711043c
Kevin Fenger <fenger@neon-lab.com>
2016-08-23 16:57:36 -0700

    iterative

--- src/js/components/pages/CollectionsMainPage.js
+++ src/js/components/pages/CollectionsMainPage.js
@@ -232,16 +232,35 @@ const CollectionsMainPage = React.createClass({
         return _(this.state.tags)
             .orderBy(['created'], ['desc'])
             // Filter hidden and empty tags.
-            .filter(t => {return t.hidden !== true && t.thumbnail_ids.length > 0})
+            .filter(this.isShowableTag)
             .slice(offset, pageSize + offset)
             .map(t => {return t.tag_id;})
             .value();
     },
 
+    isShowableTag: function(tag) {
+	if (tag.hidden === true) {
+	    return false;
+        }
+        if (tag.tag_type === UTILS.TAG_TYPE_VIDEO_COL) {
+            const video = this.state.videos[tag.video_id];
+            // We show a special placeholder component for these states.
+            if (['processing', 'failed'].includes(video.state)) {
+                return true;
+            }
+            console.log("DO we make it here"); 
+            console.log(tag); 
+        }
+        return tag.thumbnail_ids.length > 0;
+    },
+
     getVideoStatus: function(videoId) {
         var self = this;
+        console.log("BLAM"); 
+        console.log(videoId); 
         self.GET('videos', {data: {video_id: videoId, fields: UTILS.VIDEO_FIELDS}})
             .then(function(res) {
+                console.log(res.videos); 
                 res.videos[0].state === 'processed' || res.videos[0].state === 'failed' ? LoadActions.loadVideos([videoId]) : setTimeout(function() {self.getVideoStatus(videoId);}, 30000);
             })
             .catch(function(err) {
