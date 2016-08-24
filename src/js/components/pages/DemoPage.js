eda3414fb7d4e09a12471ab96ad0d5391fc6b844
scaulfield01 <caulfield@neon-lab.com>
2016-08-23 17:08:28 -0700

    clean up before adding limits logic

--- src/js/components/pages/DemoPage.js
+++ src/js/components/pages/DemoPage.js
@@ -17,18 +17,19 @@ var DemoPage = React.createClass({
     componentWillMount: function() {
         var self = this;
         if (SESSION.active()) {
-            self.context.router.replace(UTILS.DRY_NAV.VIDEO_LIBRARY.URL);
+            self.context.router.replace(UTILS.DRY_NAV.COLLLECTIONS_MAIN.URL);
         } else {
             self.POST('accounts', {
                 host: CONFIG.AUTH_HOST
             })
             .then(function (res) {
+                debugger
                 SESSION.set(res.access_token, res.refresh_token, res.account_ids[0]);
                 debugger
                 self.context.router.replace(UTILS.DRY_NAV.ONBOARDING_UPLOAD.URL);
             })
             .catch(function (err) {
-                self.context.router.push(UTILS.DRY_NAV.COLLLECTIONS_MAIN.URL);
+                self.context.router.push(UTILS.DRY_NAV.HOME.URL);
             });
         }
     },
