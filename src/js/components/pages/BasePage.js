ba05d0942c690c72075ea0995c35181255b1abbd
Nate Kresge <kresge@neon-lab.com>
2016-08-25 23:21:24 -0700

    Tweaking zoom on non emulated mobile

--- src/js/components/pages/BasePage.js
+++ src/js/components/pages/BasePage.js
@@ -63,7 +63,7 @@ class BasePage extends React.Component {
     getMeta() {
         return this.props.meta.concat([{
             name: 'viewport',
-            content: 'width=device-width, initial-scale=1.0',
+            content: 'width=device-width, initial-scale=1, maximum-scale=1',
         }]);
     }
 
