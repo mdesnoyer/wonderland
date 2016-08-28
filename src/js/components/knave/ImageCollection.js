e9290c5c3373dd14179a4f156bb72161421c71f0
Nate Kresge <kresge@neon-lab.com>
2016-08-27 19:05:59 -0700

    Mobile regression

--- src/js/components/knave/ImageCollection.js
+++ src/js/components/knave/ImageCollection.js
@@ -162,6 +162,9 @@ export default class ImageCollection extends React.Component {
         const overrideMap = {
             'copy.lift.explanation.default': 'copy.lift.explanation.images',
         };
+        const onRightThumbnailClick = this.isSoloImage()?
+            this.onAddControlClick :
+            this.props.onThumbnailClick;
         return (
             <MobileBaseCollection
                 {...this.props}
