3867f55eacf4d835dc17eafb1333cae96fc5cd72
scaulfield01 <caulfield@neon-lab.com>
2016-08-31 12:40:25 -0700

    style error box appropriately

--- src/js/components/knave/OverlayErrorFiles.js
+++ src/js/components/knave/OverlayErrorFiles.js
@@ -5,20 +5,31 @@ var OverlayErrorFiles = React.createClass({
 	render: function() {
 		if (Array.isArray(this.props.errorFiles) && this.props.errorFiles.length > 0) {
 			return (
-				<ul className="xxOverlay-scrollbox">
+				<table className="fixed_headers">
+					<thead>
+					<tr>
+						<th>File Name</th>
+						<th>Size</th>
+						<th>Error</th>
+
+					</tr>
+					</thead>
+					<tbody>
 					{
 						this.props.errorFiles.map(function(file, id) {
 							var sizeType = file.hasOwnProperty('bytes') ? 'bytes' : 'size';
 							var message = file[sizeType] >= UTILS.MAX_IMAGE_FILE_SIZE ? 'File Over 2.5MB' : 'Invaild File Type'
 							return (
-							 <li>
-							 	<span>{file.name + ' - ' + UTILS.bytesToSize(file[sizeType])}</span>
-							 	<span>{message}</span>
-							 </li>
+							 <tr>
+							 	<td>{file.name}</td>
+							 	<td>{UTILS.bytesToSize(file[sizeType])}</td>
+							 	<td>{message}</td>
+							 </tr>
 							)
 						})
 					}
-				</ul>
+					</tbody>
+				</table>
 			);	
 		}
 		else {
@@ -30,3 +41,34 @@ var OverlayErrorFiles = React.createClass({
 })
 
 export default OverlayErrorFiles
+
+// <table> 
+// 	<tr>
+// 		<th>File Name</th>
+// 		<th>Size</th>
+// 		<th>Error</th>
+// 	</tr>
+// 	<tr>
+// 		<td>File Name</td>
+// 		<td>Size</td>
+// 		<td>Error</td>
+// 	</tr>
+
+// </table> 
+
+
+
+// <ul className="xxOverlay-scrollbox">
+// 	{
+// 		this.props.errorFiles.map(function(file, id) {
+// 			var sizeType = file.hasOwnProperty('bytes') ? 'bytes' : 'size';
+// 			var message = file[sizeType] >= UTILS.MAX_IMAGE_FILE_SIZE ? 'File Over 2.5MB' : 'Invaild File Type'
+// 			return (
+// 			 <li>
+// 			 	<span>{file.name + ' - ' + UTILS.bytesToSize(file[sizeType])}</span>
+// 			 	<span>{message}</span>
+// 			 </li>
+// 			)
+// 		})
+// 	}
+// </ul>
