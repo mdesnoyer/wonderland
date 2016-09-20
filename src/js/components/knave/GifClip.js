13ef992876894b5e63605f0b415a953d316ed620
scaulfield01 <caulfield@neon-lab.com>
2016-09-20 16:32:11 -0700

    placeholder before merge

--- src/js/components/knave/GifClip.js
+++ src/js/components/knave/GifClip.js
@@ -8,7 +8,6 @@ export default class GifClip extends React.Component {
     render() {
         var url = this.props.url; 
         var score = Math.round(this.props.score * 100); 
-        // console.log(score)
         return (
             <div className="xxGifContainer" data-score={score}>
                 <video 
