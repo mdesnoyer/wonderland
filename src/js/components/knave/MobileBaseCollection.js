f0075dc3426981eaebfdc3cb023431abe6b40aac
Nate Kresge <kresge@neon-lab.com>
2016-09-27 15:44:54 -0700

    Fix regression in mobile image collection view

--- src/js/components/knave/MobileBaseCollection.js
+++ src/js/components/knave/MobileBaseCollection.js
@@ -49,7 +49,7 @@ const MobileBaseCollection = React.createClass({
         onRightThumbnailClick: PropTypes.func,
     },
 
-    getDefaultProps: function() {
+    getDefaultProps() {
         return {
             translationOverrideMap: {},
             wrapperClassName: 'xxCollection',
@@ -59,15 +59,14 @@ const MobileBaseCollection = React.createClass({
         }
     },
 
-    getInitialState: function() {
+    getInitialState() {
         return {
             open: false,
             displayInfo: false
         };
     },
 
-    getThumbnailList: function() {
-
+    getThumbnailList() {
         if (this.props.smallThumbnails.length == 0) {
             return null;
         }
@@ -124,46 +123,46 @@ const MobileBaseCollection = React.createClass({
         return <div>{children}</div>;
     },
 
-    onRightThumbnailClick: function() {
+    onRightThumbnailClickc() {
         if (this.props.onRightThumbnailClick) {
             this.props.onRightThumbnailClick();
         } else {
-            const rightThumbnailId = this.props.rightThumbnailId.thumbnail_id;
+            const rightThumbnailId = this.props.rightFeatureThumbnail.thumbnail_id;
             this.props.onThumbnailClick(rightThumbnailId);
         }
     },
 
-    handleDisplayInfo: function() {
+    onLeftThumbnailClick() {
+        const leftThumbnailId = this.props.leftFeatureThumbnail.thumbnail_id;
+        this.props.onThumbnailClick(leftThumbnailId);
+    },
+
+    handleDisplayInfoc() {
         this.setState({ displayInfo: !this.state.displayInfo });
     },
 
-    render: function() {
+    getFeatureThumbnail(thumbnail, left = true) {
+        const leftThumbnailId = this.props.leftFeatureThumbnail.thumbnail_id;
+        const title = left ? T.get('copy.bestThumbnail') : T.get('copy.worstThumbnail');
+        const className = left ? "xxThumbnail--lowLight" : "";
+        const onClick = left ? this.onLeftThumbnailClick : this.onRightThumbnailClick;
+        return (
+            <FeatureThumbnail
+                title={title}
+                score={thumbnail.neon_score}
+                enabled={thumbnail.enabled}
+                className={className}
+                src={RENDITIONS.findRendition(thumbnail)}
+                onClick={onClick}
+                isSoloImage={!left && this.props.isSoloImage}
+            />
+        );
+    },
+
+    render() {
         // Let mapped labels be overriden.
         const unapplyOverride = UTILS.applyTranslationOverride(this.props.translationOverrideMap);
 
-        const leftThumbnailId = this.props.leftFeatureThumbnail.thumbnail_id;
-        // The main left and right feature thumbnails
-        const left = (
-            <FeatureThumbnail
-                title={T.get('copy.worstThumbnail')}
-                score={this.props.leftFeatureThumbnail.neon_score}
-                enabled={this.props.leftFeatureThumbnail.enabled} 
-                className="xxThumbnail--lowLight"
-                src={RENDITIONS.findRendition(this.props.leftFeatureThumbnail)}
-                onClick={this.props.onThumbnailClick.bind(null, leftThumbnailId)}
-            />
-        );
-        const rightThumbnailId = this.props.rightFeatureThumbnail.thumbnail_id;
-        const right = (
-            <FeatureThumbnail
-                title={T.get('copy.bestThumbnail')}
-                score={this.props.rightFeatureThumbnail.neon_score}
-                enabled={this.props.rightFeatureThumbnail.enabled} 
-                src={RENDITIONS.findRendition(this.props.rightFeatureThumbnail)}
-                onClick={this.onRightThumbnailClick}
-                isSoloImage={this.props.isSoloImage ? this.props.isSoloImage() : false} 
-            />
-        );
         const displayClassName = this.state.displayInfo ? ' xxPagingControls-next--mobileGifClosed' : ''
         const renderedMedia = !this.props.clip ? (
                 <div>
@@ -175,8 +174,8 @@ const MobileBaseCollection = React.createClass({
                         />
                     </div>
                     <div className="xxCollectionImages">
-                        {left}
-                        {right}
+                        {this.getFeatureThumbnail(this.props.leftFeatureThumbnail, true)}
+                        {this.getFeatureThumbnail(this.props.rightFeatureThumbnail, false)}
                         <Lift
                             displayThumbLift={this.props.liftValue}
                             translationOverrideMap={this.props.translationOverrideMap}
@@ -191,7 +190,7 @@ const MobileBaseCollection = React.createClass({
                         <div className={"xxPagingControls-next xxPagingControls-next--GifClip" + displayClassName} onClick={this.handleDisplayInfo}></div>
                     </div>
                     <div className="xxCollectionImages">
-                        <GifClip 
+                        <GifClip
                             url={this.props.clip.renditions[2].url}
                             score={this.props.clip.neon_score}
                             poster={this.props.clipPoster}
@@ -199,14 +198,14 @@ const MobileBaseCollection = React.createClass({
                         {
                             this.props.clipsIds.length > 1 ? (
                                 <nav className="xxPagingControls-navigation xxPagingControls-navigation--GifClip">
-                                    <div 
+                                    <div
                                         className="xxPagingControls-prev xxPagingControls-prev--GifClip" 
                                         onClick={this.props.onGifClickPrev}>
                                     </div>
                                     <div className="xxPagingControls-navigation-item xxPagingControls-item--GifClip" >
                                         {(this.props.selectedGifClip + 1) + ' of '+ this.props.clipsIds.length}
                                     </div>
-                                    <div 
+                                    <div
                                         className="xxPagingControls-next xxPagingControls-next--GifClip" 
                                         onClick={this.props.onGifClickNext}>
                                     </div>
