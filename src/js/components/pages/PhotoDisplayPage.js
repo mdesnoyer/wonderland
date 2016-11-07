d28cdbbcbe6ac7a8bf2beda4d5ae20603e8da930
Nate Kresge <kresge@neon-lab.com>
2016-11-07 15:14:09 -0800

    Add rest of the buckets

--- src/js/components/pages/PhotoDisplayPage.js
+++ src/js/components/pages/PhotoDisplayPage.js
@@ -9,7 +9,7 @@ const photos = require('../../../../data/airbnb-scores.json');
 
 class PhotoDisplayPage extends Component {
 
-    getImgs(binIndex, howMany=51) {
+    getImgs(binIndex, howMany=12) {
         return _.sampleSize(photos[binIndex], howMany).map(photo => <img src={photo[0]}/>);
     }
 
@@ -19,6 +19,38 @@ class PhotoDisplayPage extends Component {
                 <h1>90th Percentile</h1>
                 <section className="photoSection">
                     {this.getImgs(9)}
+                </section>
+                <h1>80th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(8)}
+                </section>
+                <h1>70th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(7)}
+                </section>
+                <h1>60th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(6)}
+                </section>
+                <h1>50th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(5)}
+                </section>
+                <h1>40th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(4)}
+                </section>
+                <h1>30th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(3)}
+                </section>
+                <h1>20th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(2)}
+                </section>
+                <h1>10th Percentile</h1>
+                <section className="photoSection">
+                    {this.getImgs(1)}
                 </section>
                 <h1>0th Percentile</h1>
                 <section className="photoSection">
